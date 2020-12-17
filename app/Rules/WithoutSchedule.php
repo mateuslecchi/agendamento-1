f1e9ee24b2d1469e76c456f8f831306ee3bf3f25
Matheus Salomão <contatomatheusss@gmail.com>
2020-12-17 16:14:08 -0500

    fix(rules/WithoutSchedule): corrige a consulta SQL para verificar se existe apenas agendamentos confirmados.

--- app/Rules/WithoutSchedule.php
+++ app/Rules/WithoutSchedule.php
@@ -9,15 +9,80 @@ use InvalidArgumentException;
 
 class WithoutSchedule implements Rule
 {
-
     private DateTime $start_date;
     private DateTime $end_date;
 
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
+    private int $count;
+
+    private const Query = '
+    SELECT
+        COUNT(id) AS count
+    FROM
+        ((SELECT
+            id
+        FROM
+            schedules
+        WHERE
+            schedules.environments_id = ?
+                AND schedules.date = ?
+                AND situations_id IN (1)
+                AND (? BETWEEN (SELECT
+                    schedules.start_time
+                FROM
+                    schedules
+                WHERE
+                    schedules.environments_id = ?
+                        AND schedules.date = ?
+                        AND situations_id IN (1)
+                ORDER BY schedules.start_time ASC , schedules.end_time DESC
+                LIMIT 0 , 1) AND (SELECT
+                    schedules.end_time
+                FROM
+                    schedules
+                WHERE
+                    schedules.environments_id = ?
+                        AND schedules.date = ?
+                        AND situations_id IN (1)
+                ORDER BY schedules.end_time DESC , schedules.start_time ASC
+                LIMIT 0 , 1))) UNION DISTINCT (SELECT
+            id
+        FROM
+            schedules
+        WHERE
+            schedules.environments_id = ?
+                AND schedules.date = ?
+                AND situations_id IN (1)
+                AND (? BETWEEN (SELECT
+                    schedules.start_time
+                FROM
+                    schedules
+                WHERE
+                    schedules.environments_id = ?
+                        AND schedules.date = ?
+                        AND situations_id IN (1)
+                ORDER BY schedules.start_time ASC , schedules.end_time DESC
+                LIMIT 0 , 1) AND (SELECT
+                    schedules.end_time
+                FROM
+                    schedules
+                WHERE
+                    schedules.environments_id = ?
+                        AND schedules.date = ?
+                        AND situations_id IN (1)
+                ORDER BY schedules.end_time DESC , schedules.start_time ASC
+                LIMIT 0 , 1))) UNION DISTINCT (SELECT
+            id
+        FROM
+            schedules
+        WHERE
+            schedules.environments_id = ?
+                AND schedules.date = ?
+                AND situations_id IN (1)
+                AND (start_time BETWEEN ? AND ?
+                OR end_time BETWEEN ? AND ?)
+        LIMIT 0 , 1)) AS result;
+    ';
+
     public function __construct(string $date, string $start_time, string $end_time)
     {
         if (DateTime::createFromFormat('Y-m-d', $date) === false) {
@@ -36,82 +101,16 @@ class WithoutSchedule implements Rule
         $this->end_date = DateTime::createFromFormat('Y-m-d H:i:s', "{$date} {$end_time}:00");
     }
 
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param string $attribute
-     * @param mixed $value
-     * @return bool
-     */
-    public function passes($attribute, $value)
+    public function passes($attribute, $value): bool
     {
+        $this->count = 0;
         $environment_id = $value;
         $date = $this->start_date->format('Y-m-d');
         $start_time = $this->start_date->format('H:i:s');
         $end_time = $this->end_date->format('H:i:s');
 
         $result = DB::select(
-            DB::raw(
-                'SELECT
-                    COUNT(id) AS count
-                FROM
-                    ((SELECT
-                        id
-                    FROM
-                        schedules
-                    WHERE
-                        schedules.environments_id = ?
-                            AND schedules.date = ?
-                            AND (situations_id = 1 OR situations_id = 2)
-                            AND (? BETWEEN (SELECT
-                                schedules.start_time
-                            FROM
-                                schedules
-                            WHERE
-                                schedules.environments_id = ?
-                                    AND schedules.date = ?
-                            ORDER BY schedules.start_time ASC , schedules.end_time DESC
-                            LIMIT 0 , 1) AND (SELECT
-                                schedules.end_time
-                            FROM
-                                schedules
-                            WHERE
-                                schedules.environments_id = ?
-                                    AND schedules.date = ?
-                            ORDER BY schedules.end_time DESC , schedules.start_time ASC
-                            LIMIT 0 , 1))) UNION DISTINCT (SELECT
-                        id
-                    FROM
-                        schedules
-                    WHERE
-                        schedules.environments_id = ?
-                            AND schedules.date = ?
-                            AND (? BETWEEN (SELECT
-                                schedules.start_time
-                            FROM
-                                schedules
-                            WHERE
-                                schedules.environments_id = ?
-                                    AND schedules.date = ?
-                            ORDER BY schedules.start_time ASC , schedules.end_time DESC
-                            LIMIT 0 , 1) AND (SELECT
-                                schedules.end_time
-                            FROM
-                                schedules
-                            WHERE
-                                schedules.environments_id = ?
-                                    AND schedules.date = ?
-                            ORDER BY schedules.end_time DESC , schedules.start_time ASC
-                            LIMIT 0 , 1))) UNION DISTINCT (SELECT
-                        id
-                    FROM
-                        schedules
-                    WHERE
-                        schedules.environments_id = ?
-                            AND schedules.date = ?
-                            AND (start_time BETWEEN ? AND ?
-                            OR end_time BETWEEN ? AND ?) LIMIT 0 , 1)) AS result'
-            ),
+            DB::raw(self::Query),
             [
                 $environment_id, $date, $end_time, $environment_id, $date, $environment_id, $date,
                 $environment_id, $date, $start_time, $environment_id, $date, $environment_id, $date,
@@ -119,16 +118,12 @@ class WithoutSchedule implements Rule
             ]
         );
 
+        $this->count = $result[0]->count;
         return !$result[0]->count;
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
+    public function message(): string
     {
-        return 'Local já reservado nessa data e horario';
+        return __('text.rule.without-schedule.message', ['count' => $this->count]);
     }
 }
