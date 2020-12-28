2673f5d7bf656778147587fd77a9d804ec27d2dd
Matheus Salomao <contatomatheusss@gmail.com>
2020-12-28 17:42:51 -0500

    ajustes na interface

--- resources/views/livewire/dashboard/show.blade.php
+++ resources/views/livewire/dashboard/show.blade.php
@@ -5,9 +5,9 @@
                 <select id="block"
                         class="w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         wire:model="block.id" wire:change="$set('environment.id', 0)">
-                    <option value="0" selected>{{ __('todos os agendamentos') }}</option>
+                    <option value="0" selected>{{ \App\Traits\Fmt::text('label.schedule.all.in') }}</option>
                     @foreach($blocks as $block)
-                        <option value="{{ $block->id }}">{{ Str::ucfirst(__($block->name)) }}</option>
+                        <option value="{{ $block->id }}">{{ \App\Traits\Fmt::text($block->name) }}</option>
                     @endforeach
                 </select>
             </div>
@@ -15,10 +15,10 @@
             <div class="w-1/4" style="margin-top: -1px">
                 <select id="environment"
                         class="w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        wire:model="environment.id">
-                    <option value="0" selected>{{ __('label.environments.all') }}</option>
+                        wire:model="environment.id" {{ $environments->count() === 0 ? 'disabled' : '' }}>
+                    <option value="0" selected>{{ \App\Traits\Fmt::text('label.environments.all') }}</option>
                     @foreach($environments as $environment)
-                        <option value="{{ $environment->id }}">{{ Str::ucfirst(__($environment->name)) }}</option>
+                        <option value="{{ $environment->id }}">{{ \App\Traits\Fmt::text($environment->name) }}</option>
                     @endforeach
                 </select>
             </div>
@@ -27,10 +27,10 @@
                 <select id="environment"
                         class="w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         wire:model="situation">
-                    <option value="0" selected>{{ __('em qualquer situação') }}</option>
+                    <option value="0" selected>{{ \App\Traits\Fmt::text('label.situation.all') }}</option>
                     @foreach(\App\Domain\Enum\Situation::values() as $situation)
                         <option
-                            value="{{ $situation->getValue() }}">{{ Str::ucfirst(__($situation->getName())) }}</option>
+                            value="{{ $situation->getValue() }}">{{ \App\Traits\Fmt::text($situation->getName()) }}</option>
                     @endforeach
                 </select>
             </div>
@@ -45,47 +45,63 @@
     <div wire:poll.5s>
         <x-table>
             <x-slot name="head">
-                <x-table.heading>{{ Str::ucfirst(__('label.name')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.environments')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.block')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.date')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.time.schedule')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.situation')) }}</x-table.heading>
-                <x-table.heading>{{ Str::ucfirst(__('label.action')) }}</x-table.heading>
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.name') }}</x-table.heading>
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.environments') }}</x-table.heading>
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.block') }}</x-table.heading>
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.date') }}</x-table.heading>
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.time.schedule') }}</x-table.heading>
+                {{--<x-table.heading>{{ \App\Traits\Fmt::text('label.situation') }}</x-table.heading>--}}
+                <x-table.heading>{{ \App\Traits\Fmt::text('label.action') }}</x-table.heading>
             </x-slot>
             <!-- rows -->
             <x-slot name="body">
                 @forelse($schedules as $schedule)
                     <x-table.row>
-                        <x-table.cell>{{ Str::ucfirst(__($schedule->forGroup()->name)) }}</x-table.cell>
-                        <x-table.cell>{{ Str::ucfirst(__($schedule->environment?->name)) }}</x-table.cell>
-                        <x-table.cell>{{ Str::ucfirst(__($schedule->environment?->block?->name)) }}</x-table.cell>
+                        <x-table.cell>{{ $schedule->forGroup()->formattedName }}</x-table.cell>
+                        <x-table.cell>{{ $schedule->environment->formattedName }}</x-table.cell>
+                        <x-table.cell>{{ $schedule->environment->block->formattedName }}</x-table.cell>
 
                         <x-table.cell
                             class="text-center">{{ Carbon\Carbon::parse($schedule->date)->format('d/m/Y') }}
                         </x-table.cell>
 
                         <x-table.cell class="text-center">
-                            {{ Str::replaceLast(':00', '', $schedule->start_time) }}
-                            às {{ Str::replaceLast(':00', '', $schedule->end_time) }}
+                            {{
+                                \App\Traits\Fmt::text('label.schedule.time', [
+                                    'start' => \Illuminate\Support\Str::replaceLast(':00', '', $schedule->start_time),
+                                    'end' => \Illuminate\Support\Str::replaceLast(':00', '', $schedule->end_time)
+                                ])
+                            }}
                         </x-table.cell>
 
-                        <x-table.cell class="text-center">
-                            {{ Str::ucfirst(__($schedule->situation?->name)) }}
-                        </x-table.cell>
+                        {{--<x-table.cell class="text-center">
+                            <div class="font-semibold">
+                                @if($schedule->situation->id === \App\Domain\Enum\Situation::CONFIRMED()->getValue())
+                                    <span class="text-green-500">{{ Str::ucfirst(__($schedule->situation?->name)) }}</span>
+                                @elseif($schedule->situation->id === \App\Domain\Enum\Situation::PENDING()->getValue())
+                                    <span class="text-yellow-500">{{ Str::ucfirst(__($schedule->situation?->name)) }}</span>
+                                @endif
+                            </div>
+                        </x-table.cell>--}}
 
                         <x-table.cell class="text-center">
                             @switch($schedule->situation?->id)
                                 @case(\App\Domain\Enum\Situation::CONFIRMED()->getValue())
                                 <x-button.danger type="button"
-                                                 wire:click="$emit('show_schedule_cancel_modal', {{ $schedule?->id }})">{{ __('label.btn.cancel') }}</x-button.danger>
+                                                 wire:click="$emit('show_schedule_cancel_modal', {{ $schedule?->id }})">
+                                    <x-icon.cancel class="w-4 h-4 mr-1">
+                                        {{ \App\Traits\Fmt::text('label.btn.cancel') }}
+                                    </x-icon.cancel>
+                                </x-button.danger>
                                 @break
                                 @case(\App\Domain\Enum\Situation::PENDING()->getValue())
                                 <x-button type="button"
-                                          wire:click="$emit('show_schedule_confirm_modal', {{ $schedule?->id }})">{{ __('label.btn.analyze') }}</x-button>
+                                          wire:click="$emit('show_schedule_confirm_modal', {{ $schedule?->id }})">
+                                    <x-icon.clipboard class="w-4 h-4 mr-1">
+                                        {{ \App\Traits\Fmt::text('label.btn.analyze') }}
+                                    </x-icon.clipboard>
+                                </x-button>
                                 @break
-                                @default
-                                <x-button.danger type="button" disabled>{{ __('label.btn.cancel') }}</x-button.danger>
                             @endswitch
                         </x-table.cell>
                     </x-table.row>
@@ -94,7 +110,7 @@
                         <x-table.cell colspan="7">
                             <div class="flex justify-center items-center">
                                 <div class="font-medium py-8 text-cool-gray-400 text-xl">
-                                    {{ __('text.no-record-found') }}
+                                    {{ \App\Traits\Fmt::text('text.no-record-found') }}
                                 </div>
                             </div>
                         </x-table.cell>
