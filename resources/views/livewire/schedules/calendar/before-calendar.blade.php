58d0916c3770bd7b3febe1ad743f94fb37fd5e1a
Matheus Salomao <contatomatheusss@gmail.com>
2021-01-02 14:56:44 -0500

    add arrow icons

--- resources/views/livewire/schedules/calendar/before-calendar.blade.php
+++ resources/views/livewire/schedules/calendar/before-calendar.blade.php
@@ -24,11 +24,7 @@
 
     <div class="bg-white flex justify-between items-center mb-5 mt-5 p-3 border rounded-md">
         <x-button type="button" wire:click="goToPreviousMonth">
-            <div class="w-7 h-5 flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
-                </svg>
-            </div>
+            <x-icon.arrow-left class="w-4 h-4 flex justify-center"></x-icon.arrow-left>
         </x-button>
 
         <span class="font-semibold md:text-2xl">
@@ -36,12 +32,7 @@
         </span>
 
         <x-button type="button" wire:click="goToNextMonth">
-            <div class="w-7 h-5 flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                    <path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                </svg>
-            </div>
+            <x-icon.arrow-right class="w-4 h-4 flex justify-center"></x-icon.arrow-right>
         </x-button>
     </div>
 </div>
